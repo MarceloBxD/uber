02005518928a6b57ec2ac3e02839fe3f8f00f043
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-02-07 13:45:52 -0300

    Add Google Maps API key and dependencies for geocoding and map directions

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { StatusBar } from "expo-status-bar";
 import * as Location from "expo-location";
 import { useNavigation } from "@react-navigation/native";
@@ -6,20 +6,43 @@ import { useNavigation } from "@react-navigation/native";
 import * as C from "./style";
 
 import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
 import { PROVIDER_GOOGLE } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_APIKEY } from "../../config";
+import Geocoder from "react-native-geocoding";
+import { View } from "react-native";
 
 export default () => {
   const navigation = useNavigation();
-  const [location, setLocation] = useState<null | {
+  const mapRef = useRef<MapView>(null);
+
+  const [location, setLocation] = useState<{
     latitude: number;
     longitude: number;
-  }>(null);
-  const [errorMsg, setErrorMsg] = useState<string | null>(null);
-  const [latitude, setLatitude] = useState<number | null>(null);
-  const [longitude, setLongitude] = useState<number | null>(null);
+  }>({ latitude: 0, longitude: 0 });
+  const [latitude, setLatitude] = useState<number>(0);
+  const [longitude, setLongitude] = useState<number>(0);
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const [fromLocationName, setFromLocationName] = useState<string>("");
+  const [toLocationName, setToLocationName] = useState<string>("");
+  const [fromLocation, setFromLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>({
+    latitude: -22.5303564,
+    longitude: -41.932257,
+  });
+  const [toLocation, setToLocation] = useState<
+    | {
+        latitude: number;
+        longitude: number;
+      }
+    | undefined
+  >(undefined);
 
-  useEffect(() => {
-    (async () => {
+  const getMyCurrentPosition = async () => {
+    try {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
@@ -27,16 +50,63 @@ export default () => {
       }
 
       let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+
       setLatitude(location.coords.latitude);
       setLongitude(location.coords.longitude);
-      setLocation(location.coords);
-    })();
+      setFromLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+
+      const geo = await Geocoder.from(latitude, longitude);
+      if (geo.results.length > 0) {
+        const address = geo.results[0].formatted_address;
+        console.log("endereço", address);
+        setFromLocationName(address);
+      }
+    } catch (error) {
+      console.log("getMyCurrentPosition -> error", error);
+      setErrorMsg("Não foi possível obter sua localização");
+      alert("Não foi possível obter sua localização");
+    }
+  };
+
+  useEffect(() => {
+    Geocoder.init(GOOGLE_MAPS_APIKEY, { language: "pt-br" });
+    getMyCurrentPosition();
   }, []);
+
+  // const fillAddress = async () => {
+  //   if (fromLocation.latitude !== 0) {
+  //     const geo = await Geocoder.from(latitude, longitude);
+  //     console.log("geo", geo);
+
+  //     if (geo.results.length > 0) {
+  //       const address = geo.results[0].formatted_address;
+  //       console.log("endereço", address);
+  //       setFromLocationName(address);
+  //     }
+  //   }
+  // };
+
+  // fillAddress();
 
   return (
     <C.Container>
       <StatusBar style="light" backgroundColor="#000" />
       <MapView
+        initialCamera={{
+          center: {
+            latitude: 0,
+            longitude: 0,
+          },
+          pitch: 0,
+          heading: 0,
+          altitude: 0,
+          zoom: 16,
+        }}
+        ref={mapRef}
         style={{
           width: "100%",
           height: "100%",
@@ -52,7 +122,44 @@ export default () => {
           zoom: 16,
         }}
         provider={PROVIDER_GOOGLE}
-      />
+      >
+        <Marker
+          coordinate={fromLocation}
+          title="Você está aqui"
+          pinColor="#000"
+          description="Sua localização atual"
+        />
+        <MapViewDirections
+          origin={fromLocation}
+          destination={toLocation}
+          apikey={GOOGLE_MAPS_APIKEY}
+        />
+      </MapView>
+
+      <C.ItineraryArea>
+        <C.ItineraryItem>
+          <>
+            <C.ItineraryLabel>
+              <C.ItineraryPoint color="#000" />
+              <C.ItineraryTitle>Origem</C.ItineraryTitle>
+            </C.ItineraryLabel>
+            <C.ItineraryValue>
+              {fromLocationName || "Carregando..."}
+            </C.ItineraryValue>
+          </>
+        </C.ItineraryItem>
+        <C.ItineraryItem>
+          <>
+            <C.ItineraryLabel>
+              <C.ItineraryPoint color="#000" />
+              <C.ItineraryTitle>Destino</C.ItineraryTitle>
+            </C.ItineraryLabel>
+            <C.ItineraryValue>
+              {toLocationName || "Carregando..."}
+            </C.ItineraryValue>
+          </>
+        </C.ItineraryItem>
+      </C.ItineraryArea>
     </C.Container>
   );
 };
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -12,3 +12,41 @@ export const Title = styled.Text`
   margin-bottom: 24px;
   color: #000;
 `;
+
+export const ItineraryArea = styled.View`
+  position: absolute;
+  left: 10px;
+  right: 10px;
+  bottom: 10px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0px 2px 4px #999;
+  border-color: #eee;
+  border-width: 1px;
+`;
+
+export const ItineraryItem = styled.TouchableHighlight`
+  padding: 15px 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #eee;
+`;
+
+export const ItineraryLabel = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const ItineraryPoint = styled.View`
+  background-color: ${(props: { color: string }) => props.color};
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+`;
+export const ItineraryTitle = styled.Text`
+  margin-left: 10px;
+  color: #999;
+`;
+export const ItineraryValue = styled.Text`
+  font-size: 16px;
+  color: #000;
+  text-align: center;
+`;
